e0bfcd5a028f464315e767271018e90feed06181
Prez Cannady <revprez@opencorrelate.org>
2015-06-16 14:43:25 -0400

    First commit

---
+++ CommandLineC/CommandLineC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CommandLineC
+//
+//  Created by Presley Cannady on 6/16/15.
+//  Copyright (c) 2015 Universal Secure Registry, Ltd. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ HelloWorldCPP/HelloWorldCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorldCPP
+//
+//  Created by Presley Cannady on 6/16/15.
+//  Copyright (c) 2015 Universal Secure Registry, Ltd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
